3f025a5a47e5ca7942cc65786cef284c6d38ab8f
DOLLOR <8869507+DOLLOR@users.noreply.github.com>
2022-07-21 16:47:17 +0800

    Update httpServer.dart

--- httpServer.dart
+++ httpServer.dart
@@ -1,25 +1,30 @@
 import 'dart:io' as io;
 import 'dart:convert' as convert;
 
-typedef Future<String> OnRequest(io.HttpRequest request);
+typedef Future<ResponseResult> OnRequest(io.HttpRequest request);
 
 /**
  * create an http server
  */
-Future<io.HttpServer> createServer(
-    dynamic address, int port, OnRequest onRequest) async {
+Future<io.HttpServer> createServer({
+  required int port,
+  required OnRequest onRequest,
+  dynamic address,
+}) async {
+  if (address == null) {
+    address = io.InternetAddress.anyIPv6;
+  }
+
   final server = await io.HttpServer.bind(address, port);
   print('server start http://localhost:${port}');
   // handle request
   server.forEach((request) async {
     final res = await onRequest(request);
-    request.response.statusCode = 200;
-    request.response.headers.add('Server', 'dart/${io.Platform.version}');
-    request.response.headers.add('Access-Control-Allow-Origin', '*');
-    request.response.headers.add('Access-Control-Allow-Headers', '*');
-    request.response.headers.add('Cache-Control', 'public, max-age=0');
-    request.response.headers.add('Content-Type', 'text/plain; charset=UTF-8');
-    request.response.write(res);
+    request.response.statusCode = res.statusCode;
+    res.headers.forEach((key, value) {
+      request.response.headers.add(key, value);
+    });
+    request.response.write(res.data);
     request.response.close();
   });
   return server;
@@ -27,4 +32,29 @@ Future<io.HttpServer> createServer(
 
 Future<String> getRequestBody(io.HttpRequest request) {
   return convert.utf8.decoder.bind(request).join();
+}
+
+class ResponseResult {
+  dynamic data = null;
+  Map<String, String> headers = {
+    'Server': 'dart/${io.Platform.version}',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Cache-Control': 'public, max-age=0',
+    'Content-Type': 'text/plain; charset=UTF-8',
+  };
+  int statusCode = 200;
+  ResponseResult({
+    dynamic data,
+    Map<String, String>? headers,
+    int? statusCode,
+  }) {
+    this.data = data;
+    if (headers != null) {
+      this.headers = headers;
+    }
+    if (statusCode != null) {
+      this.statusCode = statusCode;
+    }
+  }
 }
