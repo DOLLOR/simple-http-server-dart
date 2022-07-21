da0aecd7787d535ca85b0cde338807cfb2d5c87d
DOLLOR <8869507+DOLLOR@users.noreply.github.com>
2022-07-21 16:48:27 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,33 +1,28 @@
 import './httpServer.dart' as httpServer;
-import 'dart:io' as io;
 
 main() async {
   const port = 9301;
-  await httpServer.createServer(io.InternetAddress.anyIPv6, port, onRequest);
+  await httpServer.createServer(port: port, onRequest: onRequest);
 }
 
-/**
- * onRequest
- * ```js
-
-    // @ts-check
-    fetch('http://localhost:9301/path/name?a=b',{
-      method:'POST',
-      body: JSON.stringify({
-        a: 1,
-        b: 'a',
-      }),
-    })
-    .then(i=>i.text())
-    .then(i=>console.log(i))
-
- * ```
- */
+/// onRequest
+/// ```js
+/// // @ts-check
+/// fetch('http://localhost:9301/path/name?a=b', {
+///   method: 'POST',
+///   body: JSON.stringify({
+///     a: 1,
+///     b: 'a',
+///   }),
+/// })
+///   .then(i => i.text())
+///   .then(i => console.log(i))
+/// ```
 final httpServer.OnRequest onRequest = (request) async {
   final postBody = await httpServer.getRequestBody(request);
   print(DateTime.now().toString());
   print(request.method);
   print(request.uri);
   print('post body: ${postBody}');
-  return 'dart http server ok: ${postBody}';
+  return httpServer.ResponseResult(data: 'dart http server ok: ${postBody}');
 };
