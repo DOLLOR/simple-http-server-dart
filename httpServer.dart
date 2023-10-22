8803d49b2509be94a6b5fb2b13efe8b1382dcfa5
DOLLOR <8869507+DOLLOR@users.noreply.github.com>
2023-10-22 21:34:39 +0800

    Update httpServer.dart

--- httpServer.dart
+++ httpServer.dart
@@ -1,7 +1,9 @@
 import 'dart:io' as io;
 import 'dart:convert' as convert;
+import 'dart:typed_data' show Uint8List;
 
 typedef Future<ResponseResult> OnRequest(io.HttpRequest request);
+// typedef OnRequest = Future<ResponseResult> Function(io.HttpRequest request);
 
 /**
  * create an http server
@@ -9,14 +11,27 @@ typedef Future<ResponseResult> OnRequest(io.HttpRequest request);
 Future<io.HttpServer> createServer({
   required int port,
   required OnRequest onRequest,
-  dynamic address,
+  io.InternetAddress? address,
+  Uint8List? certificateChain,
+  Uint8List? privateKey,
 }) async {
   if (address == null) {
     address = io.InternetAddress.anyIPv6;
   }
+  final io.HttpServer server;
 
-  final server = await io.HttpServer.bind(address, port);
-  print('server start http://localhost:${port}');
+  if (certificateChain != null && privateKey != null) {
+    final context = io.SecurityContext()
+      ..useCertificateChainBytes(certificateChain)
+      ..usePrivateKeyBytes(privateKey);
+
+    server = await io.HttpServer.bindSecure(address, port, context);
+    print('server start https://localhost:${port}');
+  } else {
+    server = await io.HttpServer.bind(address, port);
+    print('server start http://localhost:${port}');
+  }
+
   // handle request
   server.forEach((request) async {
     final res = await onRequest(request);
@@ -24,7 +39,8 @@ Future<io.HttpServer> createServer({
     res.headers.forEach((key, value) {
       request.response.headers.add(key, value);
     });
-    request.response.write(res.data);
+    request.response.add(res.data);
+    await request.response.flush();
     request.response.close();
   });
   return server;
@@ -57,4 +73,9 @@ class ResponseResult {
       this.statusCode = statusCode;
     }
   }
+}
+
+const strcodec = convert.Utf8Codec();
+List<int> stringToU8(String text) {
+  return strcodec.encode(text);
 }
